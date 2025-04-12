e203f0fda20112f98e97130ce935c7b190df6b53
AsmoMictian <asmodeus.mictian@gmail.com>
2025-04-12 07:40:58 -0400

    Added CircleShape class

--- main.py
+++ main.py
@@ -2,9 +2,26 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick() / 1000 #Return time elapsed since last frame in seconds.
+    pygame.quit
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
