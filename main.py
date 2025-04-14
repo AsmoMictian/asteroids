d2d701e26de58008d8d0225d2df481fd28cf0826
AsmoMictian <asmodeus.mictian@gmail.com>
2025-04-14 08:13:00 -0400

    Added Groups for objects. Funcionality CONFIRMED.

--- main.py
+++ main.py
@@ -10,15 +10,25 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    hero = Player((SCREEN_WIDTH / 2), SCREEN_HEIGHT / 2)
+        
+    #Create Drawable and Updatable groups for objects.
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    #Set BOTH groups as containers for Player
+    Player.containers = (updateable, drawable)
+
+    hero = Player((SCREEN_WIDTH / 2), SCREEN_HEIGHT / 2) #Player's starting position
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        hero.update(dt)
-        hero.draw(screen)
+        updateable.update(dt) #Calling the update function on the whole group instead of ind. members.
+        #Loop over ALL objects in the drawable group and .draw() them individually.
+        for item in drawable:
+            item.draw(screen) #Using item refers to each object in the group as the loop iterates.
         pygame.display.flip()
         dt = clock.tick(60)
         #Return time elapsed since last frame in seconds.
